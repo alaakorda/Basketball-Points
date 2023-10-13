f00012e794d6e2d47f721ccbc609fa2f17f73def
alaakorda <alaakorda>
2023-10-14 00:47:23 +0200

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,11 @@ class BasketBallPoints extends StatelessWidget {
           title: Text("Points Counter"),
           backgroundColor: Colors.orange,
         ),
-        body: Column(
+        body: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
+    Column(
+      children :[
             Text("Team A",style: TextStyle(fontSize: 32),),
              Text("0",style: TextStyle(fontSize: 170),),
              ElevatedButton(
@@ -48,7 +51,54 @@ class BasketBallPoints extends StatelessWidget {
               child: Text("Add 3 Point",style: TextStyle(color: Colors.black,fontSize: 20),
               ),
               ),
-              Spacer(flex: 12,),
+              Spacer(flex: 13,),
+
+      ],
+    ),
+    
+            VerticalDivider(
+              color: Colors.grey,
+              thickness: 1,
+              indent: 20,
+              endIndent: 170,
+
+            ),
+             Column(
+              children: [
+                Text("Team B",style: TextStyle(fontSize: 32),),
+                 Text("0",style: TextStyle(fontSize: 170),),
+                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 1 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 1,),
+                  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 2 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 1,),
+                  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 3 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 12,),
+              ],
+            ),
           ],
         ),
         
