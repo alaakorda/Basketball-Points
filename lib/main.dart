f29cdd536eae91c8a576c56e1296e8cdf3af67c8
alaakorda <alaakorda>
2023-10-14 01:24:33 +0200

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -64,7 +64,42 @@ class BasketBallPoints extends StatelessWidget {
               endIndent: 170,
 
             ),
-       
+             Column(
+              children: [
+                Text("Team B",style: TextStyle(fontSize: 32),),
+                 Text("0",style: TextStyle(fontSize: 170),),
+                 ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 1 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 1,),
+                  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 2 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 1,),
+                  ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.orange,
+                    minimumSize: Size(150, 50),
+                  ),
+                  onPressed: (){}, 
+                  child: Text("Add 3 Point",style: TextStyle(color: Colors.black,fontSize: 20),
+                  ),
+                  ),
+                  Spacer(flex: 12,),
+              ],
+            ),
           ],
         ),
         
