6441bfa38e6ea95188563302d9ef4deb34d638a0
alaakorda <https://github.com/alaakorda>
2023-10-17 02:40:38 +0200

    main

--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class BasketBallPoints extends StatelessWidget {
               child: Text("Add 3 Point",style: TextStyle(color: Colors.black,fontSize: 20),
               ),
               ),
-            //  Spacer(flex: 13,),
+              Spacer(flex: 13,),
 
       ],
     ),
